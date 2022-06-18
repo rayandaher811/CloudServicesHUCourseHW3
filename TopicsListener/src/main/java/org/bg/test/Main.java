f67711c173aad2c1925c2d8ca43bbc6da183eb13
Barak Gonen <barakgonen9@gmail.com>
2022-06-18 08:39:49 -0700

    Init commit

--- TopicsListener/src/main/java/org/bg/test/Main.java
+++ TopicsListener/src/main/java/org/bg/test/Main.java
@@ -1,2 +1,35 @@
-package org.bg.test;public class Main {
+package org.bg.test;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Properties;
+
+public class Main {
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost" + ":" + "9092");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "2");
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
+        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
+        props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "30000");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.LongDeserializer");
+        KafkaConsumer consumer = new KafkaConsumer(props);
+        ArrayList<String> messages = new ArrayList<>();
+        messages.add("page-activities-monthly-count");
+        messages.add("page-creation-count");
+        consumer.subscribe(messages);
+        while (true) {
+            ConsumerRecords<String, Long> records = consumer.poll(1000);
+            for (ConsumerRecord<String, Long> record : records) {
+                System.out.println("Received message: (" + record.key() + ", " + record.value() + ") at offset " + record.offset());
+            }
+        }
+
+    }
 }
