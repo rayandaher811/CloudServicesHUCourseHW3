89ea0b41a6d4f221bd3ce0c73585a135d042279e
Barak Gonen <barakgonen9@gmail.com>
2022-06-19 12:40:54 -0700

    que 1, 2 ,3

---
+++ TopicsListener/src/main/java/org/bg/test/BasicConsumeLoop.java
@@ -0,0 +1,130 @@
+package org.bg.test;
+
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.errors.WakeupException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CountDownLatch;
+
+public class BasicConsumeLoop<K extends Serializable, V extends Serializable> implements Runnable {
+
+    private static final Logger logger = LoggerFactory.getLogger(BasicConsumeLoop.class);
+
+    private final KafkaConsumer<K, V> consumer;
+    private final List<String> topics;
+    private final String clientId;
+
+    private CountDownLatch shutdownLatch = new CountDownLatch(1);
+    private ConcurrentHashMap<String, TopicStatus> topicsToStatus;
+
+
+    public BasicConsumeLoop(Properties configs, List<String> topics, ConcurrentHashMap<String, TopicStatus> topicsToStatus) {
+        this.clientId = configs.getProperty(ConsumerConfig.CLIENT_ID_CONFIG);
+        this.topics = topics;
+        this.consumer = new KafkaConsumer<>(configs);
+        this.topicsToStatus = topicsToStatus;
+    }
+
+    @Override
+    public void run() {
+
+        try {
+            logger.info("Starting the Consumer : {}", clientId);
+
+            ConsumerRebalanceListener listener = new ConsumerRebalanceListener() {
+                @Override
+                public void onPartitionsRevoked(Collection<TopicPartition> partitions) {
+                    logger.info("C : {}, Revoked partitionns : {}", (Object) clientId, partitions);
+                }
+
+                @Override
+                public void onPartitionsAssigned(Collection<TopicPartition> partitions) {
+                    logger.info("C : {}, Assigned partitions : {}", clientId, partitions);
+                }
+            };
+            consumer.subscribe(topics, listener);
+
+            logger.info("C : {}, Started to process records", clientId);
+            while(true) {
+                ConsumerRecords<K, V> records = consumer.poll(Long.MAX_VALUE);
+
+                if(records.isEmpty()) {
+                    logger.info("C : {}, Found no records", clientId);
+                    continue;
+                }
+
+                logger.info("C : {} Total No. of records received : {}", clientId, records.count());
+                for (ConsumerRecord<K, V> record : records) {
+                    logger.info("C : {}, Record received partition : {}, key : {}, value : {}, offset : {}",
+                            clientId, record.partition(), record.key(), record.value(), record.offset());
+                    if (record.topic().equals("page-creation-count") ||
+                        record.topic().equals("page-update-count") ||
+                        record.topic().equals("page-revert-action-count")) {
+                            if (topicsToStatus.containsKey(record.topic())) {
+                                topicsToStatus.get(record.topic()).setCounter((Long) record.value());
+                            } else {
+                                System.out.println("BG error!!");
+                                System.out.println("Received message: (" + record.key() + ", " + record.value() + ") at offset " + record.offset());
+                            }
+                        } else if (record.topic().equals("user-activities-count") || record.topic().equals("page-activities-count")) {
+                            if (topicsToStatus.containsKey(record.topic())) {
+                                topicsToStatus.get(record.topic()).addResult((String) record.key(), (Long) record.value());
+                            } else {
+                                System.out.println("BG error!!");
+                                System.out.println("Received message: (" + record.key() + ", " + record.value() + ") at offset " + record.offset());
+                            }
+                        }
+                    sleep(50);
+                }
+
+
+                // `enable.auto.commit` set to true. coordinator automatically commits the offsets
+                // returned on the last poll(long) for all the subscribed list of topics and partitions
+            }
+        } catch (WakeupException e) {
+            // Ignore, we're closing
+        } finally {
+            consumer.close();
+            shutdownLatch.countDown();
+            logger.info("C : {}, consumer exited", clientId);
+        }
+    }
+
+    private void sleep(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            logger.error("Error", e);
+        }
+    }
+
+    public void close() {
+        try {
+            consumer.wakeup();
+            shutdownLatch.await();
+        } catch (InterruptedException e) {
+            logger.error("Error", e);
+        }
+    }
+
+    public static Properties basicConsumerLoopConfig() {
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost" + ":" + "9092");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "2");
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
+        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
+        props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "30000");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.LongDeserializer");
+        return props;
+    }
+
+}
--- TopicsListener/src/main/java/org/bg/test/Main.java
+++ TopicsListener/src/main/java/org/bg/test/Main.java
@@ -6,30 +6,115 @@ import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.HashMap;
 import java.util.Properties;
+import java.util.Scanner;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
     public static void main(String[] args) {
-        Properties props = new Properties();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost" + ":" + "9092");
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "2");
-        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
-        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
-        props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "30000");
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.LongDeserializer");
-        KafkaConsumer consumer = new KafkaConsumer(props);
+//        Properties props = new Properties();
+//        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost" + ":" + "9092");
+//        props.put(ConsumerConfig.GROUP_ID_CONFIG, "2");
+//        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
+//        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
+//        props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "30000");
+//        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+//        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.LongDeserializer");
+//        KafkaConsumer consumer = new KafkaConsumer(props);
+
+        ConcurrentHashMap<String, TopicStatus> topicsToStatus = new ConcurrentHashMap<>();
         ArrayList<String> messages = new ArrayList<>();
-        messages.add("page-activities-monthly-count");
-        messages.add("page-creation-count");
-        consumer.subscribe(messages);
-        while (true) {
-            ConsumerRecords<String, Long> records = consumer.poll(1000);
-            for (ConsumerRecord<String, Long> record : records) {
-                System.out.println("Received message: (" + record.key() + ", " + record.value() + ") at offset " + record.offset());
+        messages.add("page-creation-count");         // for que 1.a
+        messages.add("page-update-count");           // for que 1.b
+        messages.add("page-revert-action-count");    // for que 1.c
+        messages.add("user-activities-count");       // for que 2
+        messages.add("page-activities-count");       // for que 3
+        messages.stream().forEach(s -> {
+            topicsToStatus.put(s, new TopicStatus(s));
+        });
+
+        try {
+            ExecutorService service = Executors.newFixedThreadPool(5);
+            final BasicConsumeLoop<String, Long> basicConsumeLoop = new BasicConsumeLoop<>(BasicConsumeLoop.basicConsumerLoopConfig(), messages, topicsToStatus);
+            service.execute(basicConsumeLoop);
+
+
+            System.out.println("Hello user! this is our super statitstics system");
+            System.out.println("Press 1 for total pages count");
+            System.out.println("Press 2 for total pages changes count");
+            System.out.println("Press 3 for total pages revert count");
+            System.out.println("Press 4 for top 10 most active users");
+            System.out.println("Press 5 for top 10 most active pages");
+            System.out.println("Enter 99 to stop and exit!");
+            Scanner in = new Scanner(System.in);
+            int choose = in.nextInt();
+            while (choose != 99) {
+                switch (choose) {
+                    case 1:
+                        System.out.println("Number of pages created is: " + topicsToStatus.get("page-creation-count").getCounter());
+                        break;
+                    case 2:
+                        System.out.println("Number of pages updated is: " + topicsToStatus.get("page-update-count").getCounter());
+                        break;
+                    case 3:
+                        System.out.println("Number of pages reverted is: " + topicsToStatus.get("page-revert-action-count").getCounter());
+                        break;
+                    case 4:
+                        System.out.println("10 of most active users: " + topicsToStatus.get("user-activities-count").getUserToValue());
+                        break;
+                    case 5:
+                        System.out.println("10 of most active pages: " + topicsToStatus.get("page-activities-count").getUserToValue());
+                        break;
+                    default:
+                        System.out.println("Unknown option.. please try again...");
+                }
+                System.out.println("Hello user! this is our super statitstics system");
+                System.out.println("Press 1 for total pages count");
+                System.out.println("Press 2 for total pages changes count");
+                System.out.println("Press 3 for total pages revert count");
+                System.out.println("Press 4 for top 10 most active users");
+                System.out.println("Press 5 for top 10 most active pages");
+                System.out.println("Enter 99 to stop and exit!");
+                choose = in.nextInt();
             }
+            Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+
+                @Override
+                public void run() {
+                    basicConsumeLoop.close();
+                }
+            }));
+
+        } catch (Exception e) {
+            System.exit(0);
         }
+
+
+//        while (true) {
+//            ConsumerRecords<String, Long> records = consumer.poll(1000);
+//            for (ConsumerRecord<String, Long> record : records) {
+//                if (record.topic().equals("page-creation-count") ||
+//                record.topic().equals("page-update-count") ||
+//                record.topic().equals("page-revert-action-count")) {
+//                    if (topicsToStatus.containsKey(record.topic())) {
+//                        topicsToStatus.get(record.topic()).setCounter(record.value());
+//                    } else {
+//                        System.out.println("BG error!!");
+//                        System.out.println("Received message: (" + record.key() + ", " + record.value() + ") at offset " + record.offset());
+//                    }
+//                } else if (record.topic().equals("user-activities-count") || record.topic().equals("page-activities-count")) {
+//                    if (topicsToStatus.containsKey(record.topic())) {
+//                        topicsToStatus.get(record.topic()).addResult(record.key(), record.value());
+//                    } else {
+//                        System.out.println("BG error!!");
+//                        System.out.println("Received message: (" + record.key() + ", " + record.value() + ") at offset " + record.offset());
+//                    }
+//                }
+//            }
+//        }
 
     }
 }
